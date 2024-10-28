8f2696421ed2d1cb8a494029f21e919f786e2637
Ellison Matel <111830281+Jryx0601@users.noreply.github.com>
2024-10-28 15:57:16 +0800

    Add files via upload

---
+++ Thesis Codes/Back_End/Trip_planner.py
@@ -0,0 +1,13 @@
+"""
+Planning in Trip Planning:
+1. Based on user interest like the Category
+2. trip generating 2 restaurant and 3 visits can be based on the user interest 
+
+Alternative Version:
+1. The user pick of the category like two or more on Tourist and Restaurant
+2. trip generating 2 restaurant with position like [Tour,Tour,Restau,Tour,Tour,Restau]
+- 2-3 main attractions/activities
+- 2-3 restaurants (1 lunch, 1 dinner, optional breakfast spot)
+"""
+
+
---
+++ Thesis Codes/Front_End/00_app.py
@@ -0,0 +1,25 @@
+#python -m streamlit run 00_app.py
+import streamlit as st
+
+st.set_page_config(
+    page_title="Title ng page",
+    page_icon='📃'
+)
+st.sidebar.success("Select a page")
+
+st.title("Testing app")
+st.write("Hello world")
+button1 = st.button("Click me")
+#Activate the button
+if button1:
+    st.write("ola")
+
+#Checkbox
+like = st.checkbox("Do you like ellison?")
+
+button2 = st.button("Submit")
+if button2:
+    if like:
+        st.write("Hindi ka mahal ni ellison")
+    else:
+        st.write("Nice one")
