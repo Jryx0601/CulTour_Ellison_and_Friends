5348572aeb54625ad10ba393141dd21e9c9b529b
Jryx0601 <samuraikeneki@gmail.com>
2024-11-05 22:31:31 +0800

    Front_End_Patch_Version_3

---
+++ Thesis_Codes/Back_End/Recommender_Model.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+import nltk
+import re
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, MultiLabelBinarizer
+from sklearn.metrics.pairwise import cosine_similarity
+import random
+from pathlib import Path
+script_location_dataset = Path(__file__).parent
+dataset_path = script_location_dataset/ 'Baguio_Dataset_Version2.xlsx'
+#Tourist Attraction Dataset
+data_Attraction = pd.read_excel(dataset_path, sheet_name='Tourist Attraction')
+data_Attraction_selected = data_Attraction[['Place_ID','Name','Category']]
+
+#Restaurant
+data_restaurant = pd.read_excel(dataset_path, sheet_name='Restaurant')
+data_restaurant_selected = data_restaurant[['Restaurant_ID','Name','Cuisine Type']]
+
+#For Training a model and getting recommendation system
+class recommendation_model:
+    def __init__(self):
+        self.vectorizer = TfidfVectorizer()
+    def fit(self,places,categories):
+        self.places = places
+        self.categories = categories
+        #Train the model to make cosine similarity with the help of vectorizer
+        self.tfidf_matrix = self.vectorizer.fit_transform(categories)
+        #It calculates the cosine similarity between all category vectors
+        self.cosine_sim = cosine_similarity(self.tfidf_matrix)
+    def shuffle(self,array): #It Randomize so the user don't get the same data
+        random.shuffle(array)
+        return array
+    def get_recommendation(self,place_index,n_recommendation = 5):
+        target_category = self.categories[place_index]
+        sim_scores = []
+        #places_index it should be the index of the places
+        #Only include items with matching categories
+        #It doesnt include the target
+        for idx, category in enumerate(self.categories):
+            if category == target_category and idx != place_index:
+                sim_scores.append((idx, self.cosine_sim[place_index][idx]))
+                
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+        sim_scores = self.shuffle(sim_scores)
+        sim_scores = sim_scores[:n_recommendation]
+        item_indices = [i[0] for i in sim_scores]
+        return self.places[item_indices]
+    
+#Train the two model
+"""
+Notes in this code:
+index of the places when the user interact to the place it returns the index number of the place
+"""
+#Tourist Attraction
+# Tourist = recommendation_model()
+# Tourist.fit(data_Attraction_selected['Name'],data_Attraction_selected['Category'])
+# #Restaurant Recommendation
+# Restaurant = recommendation_model()
+# Restaurant.fit(data_restaurant_selected['Name'],data_restaurant_selected['Cuisine Type'])
+
+
+# target = 3
+# sample = list(Restaurant.get_recommendation(target))
+# print(sample)
+
+"""
+Notes in Applying Recommedation System:
+1. When the user click the places below of the places are the recommended with same topic or category
+
+Notes in Upgrading or Enhancing the System:
+1. Images Array
+"""
--- Thesis_Codes/Front_End/pages/Home.py
+++ Thesis_Codes/Front_End/pages/Home.py
@@ -1,16 +1,27 @@
 #python -m streamlit run 00_app.py
 import streamlit as st
-import pandas as pd
 import numpy as np
+import pandas as pd
 import datetime
 from streamlit_folium import st_folium
 import folium
 from pathlib import Path
 import os
-import openpyxl
-from PIL import Image
-import io
+import sys
+st.set_page_config(initial_sidebar_state="collapsed")
+#---------------------------------------------------------------
+#Accessing a file in back_End
+script_recommendation = Path(__file__).parent.parent.parent
+sys.path.append(str(script_recommendation))
+from Back_End.Recommender_Model import recommendation_model, data_Attraction_selected, data_restaurant_selected
 
+#Tourist Recommendation
+Tourist = recommendation_model()
+Tourist.fit(data_Attraction_selected['Name'],data_Attraction_selected['Category'])
+#Restaurant Recommendation
+Restaurant = recommendation_model()
+Restaurant.fit(data_restaurant_selected['Name'],data_restaurant_selected['Cuisine Type'])
+#---------------------------------------------------------------
 #pip install streamlit-option-menu
 from streamlit_option_menu import option_menu
 #st.title("ColTour")
@@ -24,7 +35,7 @@ selected = option_menu(
     orientation="horizontal",
 )
 if selected == "Map":
-   #Display of map
+    #Display of map
    #Testing lang ito
 
    #Interactive map
@@ -34,40 +45,60 @@ if selected == "Map":
    #Folium map
    #pip install streamlit-folium
    #https://github.com/randyzwitch/streamlit-folium
-   m = folium.Map(location = [14.3269, 120.9575], zoom_start = 16)
-   folium.Marker(
-       [14.3269, 120.9575],
-       popup = "DLSUD",
-       tooltip = "DLSUD"
-       ).add_to(m)
+    m = folium.Map(location = [14.3269, 120.9575], zoom_start = 16)
+    folium.Marker(
+        [14.3269, 120.9575],
+        popup = "DLSUD",
+        tooltip = "DLSUD"
+        ).add_to(m)
    
-   # call to render Folium map in Streamlit
-   st_data = st_folium(m, width = 725)
+    # call to render Folium map in Streamlit
+    st_data = st_folium(m, width = 725)
 
-   st.divider()
+    st.divider()
 
    #Places
-   """
-   Testing for gathering all the image once done in the UI testing
-   """
-   script_location = Path(__file__).parent
-   sample = 'test1'
-   image_path = script_location/'images'/f'{sample}.jpg'
-   st.image(str(image_path))
-
-   
+    script_location = Path(__file__).parent
+    sample = 'test1'
+    image_path = script_location/'images'/f'{sample}.jpg'
    #Dito pagkaclick ng image lalabas ito
-   with st.popover("Image dito"):
-        st.markdown("Pic ulet dito")
-        st.divider()
-        st.markdown("Place name")
-        st.caption("Description ng place")
+#    with st.popover("Sample"):
+#         st.image(str(image_path))
+#         st.markdown("Pic ulet dito")
+#         st.divider()
+#         st.markdown("Place name")
+#         st.caption("Description ng place")
 
-   with st.popover("Image dito"):
-        st.markdown("Pic ulet dito")
-        st.divider()
-        st.markdown("Place name")
-        st.caption("Description ng place")
+#    with st.popover("Image dito"):
+#         st.markdown("Pic ulet dito")
+#         st.divider()
+#         st.markdown("Place name")
+#         st.caption("Description ng place")
+
+    st.text('Tourist Attraction')
+    #Columns of pop over
+    col1,col2,col3,col4,col5 = st.columns(5)
+    with col1:
+        st.image(str(image_path), use_column_width=True)
+        with st.popover('Name_Place'):
+            st.text('Sample')
+    with col2:
+        with st.popover('Name_Place'):
+            st.text('Sample')
+    with col3:
+        with st.popover('Name_Place'):
+            st.text('Sample')
+    with col4:
+        with st.popover('Name_Place'):
+            st.text('Sample')
+    with col5:
+        with st.popover('Name_Place'):
+            st.text('Sample')
+
+    st.divider()
+
+    st.text('Restaurant')
+
 
     
 #----------------------------------------------------------------------------------------------------
